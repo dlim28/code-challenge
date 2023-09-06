fe7948332c0a8ebcc8588bd5735d3fb9ea6d83b0
Darren <darrenlim193@gmail.com>
2023-09-06 20:55:07 +1000

    Created layout with navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { slide } from 'svelte/transition'
+	let hidden = true;
+</script>
+
+<nav class="flex flex-col">
+	<div class="bg-red-700 justify-center py-4">
+		<div on:click={() => {hidden = !hidden}}>User Icon</div>
+		{#if !hidden}
+			<ul transition:slide>
+				<li>User Option 1</li>
+				<li>User Option 2</li>
+				<li>Preferred Coin</li>
+				<li>User Option 4</li>
+			</ul>
+		{/if}
+	</div>
+</nav>
+
+<slot />
