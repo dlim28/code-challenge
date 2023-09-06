ea707aec2b6c07c5820fe7e08422d78207ef14d3
Darren <darrenlim193@gmail.com>
2023-09-06 21:02:21 +1000

    Navbar with text centered

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,18 +3,16 @@
 	let hidden = true;
 </script>
 
-<nav class="flex flex-col">
-	<div class="bg-red-700 justify-center py-4">
-		<div on:click={() => {hidden = !hidden}}>User Icon</div>
-		{#if !hidden}
-			<ul transition:slide>
-				<li>User Option 1</li>
-				<li>User Option 2</li>
-				<li>Preferred Coin</li>
-				<li>User Option 4</li>
-			</ul>
-		{/if}
-	</div>
+<nav class="flex flex-col py-4 justify-center items-center bg-red-700">
+	<button class="w-1/3" on:click={() => {hidden = !hidden}}>User Icon</button>
+	{#if !hidden}
+		<ul transition:slide>
+			<li>User Option 1</li>
+			<li>User Option 2</li>
+			<li>Preferred Coin</li>
+			<li>User Option 4</li>
+		</ul>
+	{/if}
 </nav>
 
 <slot />
