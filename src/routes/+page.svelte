1c452a173277daf3fa5bba642d1afc39d081ec7e
Darren <darrenlim193@gmail.com>
2023-09-11 17:09:59 +1000

    Updated optional chaining to fail better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,33 +44,33 @@
 			<div class="xl:col-start-1 xl:grid xl:grid-cols-2 overflow-hidden bg-gradient-to-r from-white to-gray-50 shadow-xl rounded-xl mx-2 xl:mx-4 mt-4 border-4 border-gray-100">
 				<div class="flex flex-col justify-center items-center px-4 py-6 xl:grid-start-1">
 					<img src="src/images/user-icon.svg" alt="A Placeholder" width="100" height="100">
-					<h3 class="text-lg font-semibold leading-6 text-gray-900">{$preferredCoinData?.buy || data.buy}</h3>
-					<h4 class="font-semibold text-xl italic">{$preferredCoinData.ask?.toFixed(3) || data.ask.toFixed(3)}</h4>
+					<h3 class="text-lg font-semibold leading-6 text-gray-900">{$preferredCoinData?.buy || data?.buy}</h3>
+					<h4 class="font-semibold text-xl italic">{$preferredCoinData.ask?.toFixed(3) || data.ask?.toFixed(3)}</h4>
 				</div>
 				<div class="hidden xl:flex flex-col justify-center items-center px-4 xl:grid-start-2">
 					<div class="px-2 py-2 space-y-2 w-full">
 						<div class="relative flex items-center justify-center space-x-3 rounded-lg border border-gray-300 bg-white shadow-sm hover:border-gray-400">
 							<div class="min-w-0 flex flex-col text-center p-4">
 								<p>Bid</p>
-								<p class="italic">{$preferredCoinData.bid?.toFixed(3) || data.bid.toFixed(3)}</p>
+								<p class="italic">{$preferredCoinData.bid?.toFixed(3) || data.bid?.toFixed(3)}</p>
 							</div>
 						</div>
 						<div class="relative flex items-center justify-center space-x-3 rounded-lg border border-gray-300 bg-white shadow-sm hover:border-gray-400">
 							<div class="min-w-0 flex flex-col text-center p-4">
 								<p>Rate</p>
-								<p class="italic text-sm truncate">{$preferredCoinData.rate || data.rate}</p>
+								<p class="italic text-sm truncate">{$preferredCoinData?.rate || data?.rate}</p>
 							</div>
 						</div>
 						<div class="relative flex items-center justify-center space-x-3 rounded-lg border border-gray-300 bg-white shadow-sm hover:border-gray-400">
 							<div class="min-w-0 flex flex-col text-center p-4">
 								<p>Spot Rate</p>
-								<p class="italic">{$preferredCoinData.spotRate?.toFixed(3) || data.spotRate.toFixed(3)}</p>
+								<p class="italic">{$preferredCoinData.spotRate?.toFixed(3) || data.spotRate?.toFixed(3)}</p>
 							</div>
 						</div>
 						<div class="relative flex items-center justify-center space-x-3 rounded-lg border border-gray-300 bg-white shadow-sm hover:border-gray-400">
 							<div class="min-w-0 flex flex-col text-center p-4">
 								<p>Date</p>
-								<p class="italic">{new Date($preferredCoinData.timestamp || data.timestamp).toDateString()}</p>
+								<p class="italic">{new Date($preferredCoinData.timestamp || data?.timestamp).toDateString()}</p>
 							</div>
 						</div>
 					</div>
@@ -83,8 +83,8 @@
 						{#each $recommendedCoins as coin}
 							<div class="relative flex items-center justify-center space-x-3 rounded-lg border border-gray-300 bg-white shadow-sm hover:border-gray-400">
 								<div class="min-w-0 flex flex-col text-center p-4">
-									<p>{coin.buy}</p>
-									<p class="italic">${coin.ask.toFixed(3)}</p>
+									<p>{coin?.buy}</p>
+									<p class="italic">${coin.ask?.toFixed(3)}</p>
 								</div>
 							</div>
 						{/each}
@@ -92,25 +92,25 @@
 						<div class="relative flex items-center justify-center space-x-3 rounded-lg border border-gray-300 bg-white shadow-sm hover:border-gray-400">
 							<div class="min-w-0 flex flex-col text-center p-4">
 								<p>Bid</p>
-								<p class="italic">{data.bid.toFixed(3)}</p>
+								<p class="italic">{data.bid?.toFixed(3)}</p>
 							</div>
 						</div>
 						<div class="relative flex items-center justify-center space-x-3 rounded-lg border border-gray-300 bg-white shadow-sm hover:border-gray-400">
 							<div class="min-w-0 flex flex-col text-center p-4">
 								<p>Rate</p>
-								<p class="italic text-sm truncate">{data.rate}</p>
+								<p class="italic text-sm truncate">{data?.rate}</p>
 							</div>
 						</div>
 						<div class="relative flex items-center justify-center space-x-3 rounded-lg border border-gray-300 bg-white shadow-sm hover:border-gray-400">
 							<div class="min-w-0 flex flex-col text-center p-4">
 								<p>Spot Rate</p>
-								<p class="italic">{data.spotRate.toFixed(3)}</p>
+								<p class="italic">{data.spotRate?.toFixed(3)}</p>
 							</div>
 						</div>
 						<div class="relative flex items-center justify-center space-x-3 rounded-lg border border-gray-300 bg-white shadow-sm hover:border-gray-400">
 							<div class="min-w-0 flex flex-col text-center p-4">
 								<p>Date</p>
-								<p class="italic">{new Date(data.timestamp).toDateString()}</p>
+								<p class="italic">{new Date(data?.timestamp).toDateString()}</p>
 							</div>
 						</div>
 					{/if}
