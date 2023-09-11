e42bd58600ebb108c762afd3d8e3971ec3b6afc1
Darren <darrenlim193@gmail.com>
2023-09-11 11:14:44 +1000

    Style update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 
 <style>
 	body {
-		background: #f9fafb;
+		background: #f3f4f6;
 	}
 </style>
 
@@ -18,29 +18,29 @@
 	{#await getCoin(window.localStorage.getItem('preferredCoin') ? window.localStorage.getItem('preferredCoin') : 'btc')}
 	<h1>Waiting</h1>
 	{:then data}
-		<div class="flex inset-1 flex-col rounded-xl mx-2 shadow-2xl items-center justify-center px-6 py-4 mt-6">
-			<img src="src/images/user-icon.svg" alt="A Placeholder" width="100" height="100">
-			<h1 class="text-lg">{data.buy}</h1>
-			<h2 class="font-semibold text-xl italic">{data.ask.toFixed(3)}</h2>
+		<div class="overflow-hidden bg-white shadow-xl rounded-xl mx-2 mt-2 border border-gray-100">
+			<div class="flex flex-col justify-center items-center px-4 py-6">
+				<img src="src/images/user-icon.svg" alt="A Placeholder" width="100" height="100">
+				<h3 class="text-lg font-semibold leading-6 text-gray-900">{data.buy}</h3>
+				<h4 class="font-semibold text-xl italic">{data.ask.toFixed(3)}</h4>
+			</div>
 		</div>
 
-		<div class="flex flex-col rounded-xl mx-2 shadow-2xl my-4 p-4 gap-y-2">
-			<div class="flex flex-row justify-evenly">
-				<div class="flex flex-col text-center p-4">
+		<div class="overflow-hidden bg-white shadow-xl rounded-xl mx-2 mt-2 border border-gray-100">
+			<div class="grid grid-cols-2 grid-rows-2 px-4 py-6">
+				<div class="flex flex-col text-center p-4 col-start-1 row-span-1">
 					<p>Bid</p>
 					<p class="italic">{data.bid.toFixed(3)}</p>
 				</div>
-				<div class="flex flex-col text-center p-4">
+				<div class="flex flex-col text-center p-4 col-start-2 row-span-1">
 					<p>Rate</p>
 					<p class="italic">{data.rate}</p>
 				</div>
-			</div>
-			<div class="flex flex-row justify-evenly">
-				<div class="flex flex-col text-center p-4">
+				<div class="flex flex-col text-center p-4 col-start-1 row-span-2">
 					<p>Spot Rate</p>
 					<p class="italic">{data.spotRate.toFixed(3)}</p>
 				</div>
-				<div class="flex flex-col text-center p-4">
+				<div class="flex flex-col text-center p-4 col-start-2 row-span-2">
 					<p>Date</p>
 					<p class="italic">{new Date(data.timestamp).toDateString()}</p>
 				</div>
